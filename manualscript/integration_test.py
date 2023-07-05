506fd9ac01e7ebdec4449a22018f68c07a2874fa
sumitra8836512 <spathak6512@conestogac.on.ca>
2023-07-05 11:00:38 -0400

    Lab 5

--- manualscript/integration_test.py
+++ manualscript/integration_test.py
@@ -1,25 +1,25 @@
+
 import calculator
-import unittest
+
+def test_addition():
+    assert calculator.add(2, 3) == 5
+
+def test_subtraction():
+    assert calculator.subtract(5, 3) == 2
+
+def test_multiplication():
+    assert calculator.multiply(2, 3) == 6
+
+def test_division():
+    assert calculator.divide(6, 3) == 2
+
+def test_divide_by_zero():
+    try:
+        calculator.divide(6, 0)
+    except ValueError as e:
+        assert str(e) == "Cannot divide by zero"
+    else:
+        assert False, "Expected ValueError"
 
 
-class MyTestCase(unittest.TestCase):
 
-    def test_addition(self):
-        self.assertEqual(calculator.add(2, 3), 5)
-
-    def test_subtraction(self):
-        self.assertEqual(calculator.subtract(5, 3), 2)
-
-    def test_multiplication(self):
-        self.assertEqual(calculator.multiply(2, 3), 6)
-
-    def test_division(self):
-        self.assertEqual(calculator.divide(6, 3), 2)
-
-    def test_divide_by_zero(self):
-        try:
-            calculator.divide(6, 0)
-        except ValueError as e:
-            assert str(e) == "Cannot divide by zero"
-        else:
-            assert False, "Expected ValueError"
